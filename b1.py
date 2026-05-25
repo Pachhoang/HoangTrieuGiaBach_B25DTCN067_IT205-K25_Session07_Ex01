8614abacfdddb19c8eee681f568aaced8c50ec07
Pachhoang <kiviyt2007@gmail.com>
2026-05-25 07:47:45 +0700

    btvn

---
+++ b1.py
@@ -0,0 +1,11 @@
+student_name = "  nguYEn vAn a  "
+student_code = "  rk-001-python  "
+email = "  student01@gmail.com  "
+
+student_name = student_name.strip().title()
+student_code = student_code.strip().upper()
+email = email.strip().lower()
+
+print("Họ tên:", student_name)
+print("Mã học viên:", student_code)
+print("Email:", email)
