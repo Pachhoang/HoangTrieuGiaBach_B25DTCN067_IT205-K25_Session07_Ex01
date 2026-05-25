617588412486accd6537ed2dc097754296807f8a
Pachhoang <kiviyt2007@gmail.com>
2026-05-25 20:43:07 +0700

    Update b1.py

--- b1.py
+++ b1.py
@@ -9,3 +9,13 @@ email = email.strip().lower()
 print("Họ tên:", student_name)
 print("Mã học viên:", student_code)
 print("Email:", email)
+
+#C1 Vì strip() trả về chuỗi mới nên không thay đổi trực tiếp student_name.
+
+#C2 Vì title() cũng trả về chuỗi mới, nếu không gán lại thì kết quả không đổi.
+
+#C3 Vì upper() không chỉnh sửa trực tiếp chuỗi gốc mà chỉ tạo chuỗi mới viết hoa.
+
+#C4 Vì lower() chỉ trả về chuỗi mới viết thường chứ không tự cập nhật biến email.
+
+#C5 Muốn các phương thức có hiệu lực cần gán lại giá trị cho biến.
